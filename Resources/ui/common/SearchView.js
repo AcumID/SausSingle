c11ac60ae05c1ac759b17b1caa6237adf302188d
unknown <Admin@.(none)>
2012-11-08 16:01:39 +0100

    SearchView class holds the searchbar.

---
+++ Resources/ui/common/SearchView.js
@@ -0,0 +1,37 @@
+function SearchView() {
+	
+	var self = Ti.UI.createView({
+		backgroundColor: 'black',
+		width: 200,
+		height: 55
+		
+	});
+
+	var search = Ti.UI.createTextField({
+		height: 43,
+		width: 200,
+		top: 10,
+		borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+		hintText: "Hva' leder du efter?"
+	});
+	
+	search.addEventListener('focus',function(e){
+		if(e.source.hintText=="Hva' leder du efter?"){
+			e.source.hintText="";
+		}
+	});
+	
+	search.addEventListener('change',function(e){
+		self.fireEvent(e.source,'update',e.value);
+	})
+	
+	search.addEventListener('blur',function(e){
+		e.source.hintText="Hva, leder du efter?";
+	});
+	
+	self.add(search);
+
+	return self;
+};
+	
+module.exports = SearchView;	
