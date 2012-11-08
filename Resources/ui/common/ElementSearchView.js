a2b69a15c9b268a05e51ec2a81178cc2429aed42
unknown <Admin@.(none)>
2012-11-08 16:00:21 +0100

    ElementSearchView redone to ensure OOP.

--- Resources/ui/common/ElementSearchView.js
+++ Resources/ui/common/ElementSearchView.js
@@ -7,32 +7,21 @@ function ElementSearchView() {
 		
 	});
 
-
-	// var search = Ti.UI.createSearchBar({
-    	// barColor:'#000', 
-    	// showCancel:true,
-    	// height:43,
-    	// width: 200,
-    	// top:10,
-    	// hintText: "Hva' leder du efter?"
-	// });
-	// self.add(search);	var search = Ti.UI.createTextField({
-		height: 43,
-		width: 200,
-		top: 10,
-		borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
-		hintText: "Hva' leder du efter?"
+	var SearchView = require('ui/common/ElementSearchView');
+	var search = new SearchView();
+	
+	var MatElmView = require('ui/common/MaterialElementView');	
+	search.addEventlistener('update',function(e){
+		updateView(e.value);
 	});
 	
-	search.addEventListener('focus',function(e){
-		e.source.hintText="";
-	});
+	var updateView = function (searchQuery){
+		alert(searchQuery);
+		//TODO for loop to generate matElms
+		var matElm = new MatElmView("En vare");
+		self.add(matElm);
+	};
 	
-	search.addEventListener('blur',function(e){
-		e.source.hintText="Hva, leder du efter?";
-	});
-	
-	self.add(search);
 	return self;
 };
 	
